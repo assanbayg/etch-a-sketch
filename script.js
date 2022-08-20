37871f3d1fd6677ce516dad5c893598eaf869579
Gauhar Assanbay <asanbayg@gmail.com>
2022-08-20 07:54:58 +0600

    Added step 5!

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function create(){
-
+    clear();
     for(let i = 0; i < number ** 2; ++i){
         let span = document.createElement('span');
         span.style.cssText = `background-color: white; width: ${size}px; height: ${size}px;`;
@@ -16,16 +16,19 @@ function create(){
 });
 }
 
+function clear(){
+    let spans = container.querySelectorAll('span');
+    spans.forEach(element => element.remove());
+}
+
 let button = document.querySelector('button');
 let colorInput = document.querySelector('.color');
 let color = colorInput.value;
 let numberInput = document.querySelector('.number');
 let number = numberInput.value;
-
+number.onchange = (e) => create();
 const container = document.querySelector('.container');
 
-//const number = 32;
 const size = 500 / number;
-
 
 button.addEventListener('click', (e) => create());
